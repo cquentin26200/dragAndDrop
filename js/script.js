d33a89563d3ac287e5519a6b155d51c180e812b9
cquentin26200 <qcheron@edenschool.fr>
2022-05-11 11:43:59 +0200

    try to finish drag and drop

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
-const box = document.querySelectorAll(".box");
-const redbox = document.querySelector(".redbox");
+const ball = document.querySelectorAll(".box");
+ball.forEach(element => {
+  element.onmousedown = function(event) {
+    // (1) la préparer au déplacement :  réglé en absolute et en haut par z-index
+    element.style.position = 'absolute';
+    element.style.zIndex = 1000;
+  
+    // déplacez-le de tout parent actuel directement dans body
+    // pour le placer par rapport à body
+    document.body.append(element);
+  
+    // Centrer la balle aux coordonnées (pageX, pageY)
+    function moveAt(pageX, pageY) {
+      element.style.left = pageX - element.offsetWidth / 2 + 'px';
+      element.style.top = pageY - element.offsetHeight / 2 + 'px';
+    }
+  
+    // déplacer notre balle en positionnement absolu sous le pointeur
+    moveAt(event.pageX, event.pageY);
+  
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+  
+    // (2) déplacer la balle sur le déplacement de la souris
+    document.addEventListener('mousemove', onMouseMove);
+  
+    // (3) laisser tomber la balle, retirer les gestionnaires inutiles
+    element.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      element.onmouseup = null;
+    };
+  
+  };
+})
 
-box.forEach((element) => {
-  redbox.addEventListener("click", console.log("event.target"));
-  element.addEventListener("dragenter", dragEnter);
-  element.addEventListener("dragover", dragOver);
-  element.addEventListener("dragleave", dragLeave);
-  element.addEventListener("drop", drop);
-});
-function dragEnter(e) {
-  e.target.classList.add("drag-over");
-}
-
-function dragOver(e) {
-  e.target.classList.add("drag-over");
-}
-
-function dragLeave(e) {
-  e.preventDefault();
-  e.target.classList.remove("drag-over");
-}
-
-function drop(e) {
-  e.preventDefault();
-  e.target.classList.remove("drag-over");
-}
